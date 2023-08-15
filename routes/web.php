0d91da7c324ab77848e92fd6d5d381727ec7b461
Kuldeepwcuk <141031071+Kuldeepwcuk@users.noreply.github.com>
2023-08-15 12:46:51 +0530

    Update web.php
    fff

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes-11
+| Web Routes-112233
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
